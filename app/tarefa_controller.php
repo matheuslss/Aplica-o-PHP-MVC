d080503142ebdf5ae823fb8b7bd6381c78b1de25
Matheus Lucas <matheuslucas.ssilva@gmail.com>
2019-09-16 09:08:12 -0300

    update tarefa_controller

--- app/tarefa_controller.php
+++ app/tarefa_controller.php
@@ -1,7 +1,20 @@
 <?php 
 
-    echo '<pre>';
-    print_r($_POST);
-    echo '</pre>';
+    // echo '<pre>';
+    // print_r($_POST);
+    // echo '</pre>';
+
+    require "../tarefa.model.php";
+    require "../tarefa.service.php";
+    require "../conexao.php";
+
+    $tarefa = new Tarefa();
+
+    $tarefa->__set("tarefa", $_POST["tarefa"]);
+
+    $conexao = new Conexao();
+
+    $tarefaService = new TarefaService();
+    
     
 ?>
